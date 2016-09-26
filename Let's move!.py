b951109d3d4fc1881b7e290a30568ba3537842b5
CornChen <917330462@qq.com>
2016-09-27 00:19:38 +0800

    Update Let's move!.py

--- Let's move!.py
+++ Let's move!.py
@@ -1,3 +1,7 @@
+#move the name 
+#
+#
+#
 import os,time
 i = os.system('cls')
 print(" ###  #   #   #####  #####      #     #  #   #      #    #  #   #  #####")
@@ -61,3 +65,61 @@ print("                                #     #   #   #      #   #       #   #   
 print("                                #     #####   #####  #   #         #     #   #      #       #   #  #   #")
 print("                                #     #   #   #      #   #         #     #   #      #  #    #   #  #   #")
 print("                                 ###  #   #   #####  #   #         #      ###       #    #   ###   #   #")
+
+
+#rotate the picture
+#thanks for LiPeng and CaoXinYu
+#
+i=0
+for i in range(100):
+    if i%2==0:
+        os.system('cls')
+        print('                                 #                              ')
+        print('                                 #                              ')
+        print('                                 #                              ')
+        print('                                 #                              ')
+        print('                                 #                              ')
+        print('                                 #                              ')
+        print('                                 #                              ')
+        print('                                 #                              ')
+        print('                                 #                              ')
+        print('                                 #                              ')
+        print('                                 #                              ')
+        print('          ########################                              ')
+        print('                                                                ')
+        print('                                                                ')
+        print('                                                                ')
+        print('                                                                ')
+        print('                                                                ')
+        print('                                                                ')
+        print('                                                                ')
+        print('                                                                ')
+        print('                                                                ')
+        print('                                                                ')
+        print('                                                                ')
+        time.sleep(0.1)
+    else:
+        os.system('cls')
+        print('                                                              ')
+        print('                                                              ')
+        print('                                                              ')
+        print('                                                              ')
+        print('                                                              ')
+        print('                                                              ')
+        print('                                                              ')
+        print('                                                              ')
+        print('                                                              ')
+        print('                                                              ')
+        print('                               #######################        ')
+        print('                                #                             ')
+        print('                                #                             ')
+        print('                                #                             ')
+        print('                                #                             ')
+        print('                                #                             ')
+        print('                                #                             ')
+        print('                                #                             ')
+        print('                                #                             ')
+        print('                                #                             ')
+        print('                                                              ')
+        print('                                                              ')
+        time.sleep(0.1)
